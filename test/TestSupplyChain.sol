57bd8ef48b20bc24ca2bea5ee4c91966af82efad
bryanherbert <bryan.herbert1124@gmail.com>
2018-12-04 13:57:15 -0500

    finished first iteration of tests

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -6,13 +6,12 @@ import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
 
-    /*function testConstructor() {
-        SupplyChain supplyChain = SupplyChain(DeployedAddresses.SupplyChain());
+    function testConstructor() {
 
         uint expected_sku = 0;
 
         Assert.equal(supplyChain.skuCount(), expected_sku, "Sku count is not initialized to 0");
-    }*/
+    }
 
     //State Variables
     SupplyChain supplyChain;
@@ -20,10 +19,17 @@ contract TestSupplyChain {
     Buyer buyer;
     uint skuCount;
     uint price;
+    uint state;
     uint public initialBalance = 10 ether;
+    //store return values
+    string a;
+    uint b;
+    uint c;
+    uint d;
+    address e;
+    address f;
 
     function beforeAll(){
-        //Setup - needs a constructor or a hook
         //Deploy SupplyChain, Seller and Buyer contracts
         supplyChain = new SupplyChain();
         seller = new Seller();
@@ -36,20 +42,12 @@ contract TestSupplyChain {
         string memory name = "tuxedo t-shirt";
         price = 1 ether;
         skuCount = 0;
-        uint state = 0;
+        state = 0;
         address itemSeller = address(seller);
         address itemBuyer = 0;
 
         //Have seller contract add item
         seller.addItem2supply(supplyChain, name, price);
-
-        //store return values
-        string memory a;
-        uint b;
-        uint c;
-        uint d;
-        address e;
-        address f;
         
         (a, b, c, d, e, f) = supplyChain.fetchItem(skuCount);
 
@@ -63,14 +61,14 @@ contract TestSupplyChain {
 
     }
 
-    // Test for failing conditions in this contracts
+    // Test for failing conditions in this contract
     // test that every modifier is working
 
     // buyItem
     
     function testBuyItem() {
 
-        uint state = 1;
+        state = 1;
         //Store account balances before transactions
         //Seller
         uint sellerPreBalance = address(seller).balance;
@@ -86,12 +84,7 @@ contract TestSupplyChain {
         //Buyer
         uint buyerPostBalance = address(buyer).balance;
         
-        //store return values
-        uint c;
-        uint d;
-        address f;
-        
-        (, , c, d, , f) = supplyChain.fetchItem(skuCount);
+        (a, b, c, d, e, f) = supplyChain.fetchItem(skuCount);
 
         Assert.equal(d, state, "State of last added item does not match expected value");
         Assert.equal(f, address(buyer), "Buyer of last added item is not 0");
@@ -108,13 +101,12 @@ contract TestSupplyChain {
     // shipItem
 
     function testShipItem() {
-        uint state = 2;
+        state = 2;
 
         //Have seller ship item
         seller.shipItem(supplyChain, skuCount);
 
-        uint d;
-        (, , , d, , ) = supplyChain.fetchItem(skuCount);
+        (a, b, c, d, e, f) = supplyChain.fetchItem(skuCount);
 
         Assert.equal(d, state, "State of Item should be Shipped");
     }
@@ -124,12 +116,19 @@ contract TestSupplyChain {
 
     // receiveItem
 
+    function testReceiveItem() {
+        state = 3;
+
+        //Have buyer mark item as received
+        buyer.receiveItem(supplyChain, skuCount);
+
+        (a, b, c, d, e, f) = supplyChain.fetchItem(skuCount);
+
+        Assert.equal(d, state, "State of Item should be Shipped");
+    }
+
     // test calling the function from an address that is not the buyer
     // test calling the function on an item not marked Shipped
-
-     
-
-
 }
 
 contract Seller {
@@ -139,12 +138,12 @@ contract Seller {
         return _supplyChain.addItem(_item, _price);
     }
 
-    function shipItem(SupplyChain _supplyChain, uint _sku) {
+    function shipItem(SupplyChain _supplyChain, uint _sku) public {
         
         _supplyChain.shipItem(_sku);
     }
 
-    function() payable {
+    function() public payable {
 
     }
 }
@@ -159,7 +158,12 @@ contract Buyer {
 
     }
 
-    function() payable {
+    function receiveItem(SupplyChain _supplyChain, uint _sku) public {
+
+        _supplyChain.receiveItem(_sku);
+    }
+
+    function() public payable {
 
     }
 
